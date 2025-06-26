2ead3e3f6d5681ec66b27cb1cc8f8aad0bcd03b2
bhatkayatri <yatribhatka@gmail.com>
2025-06-26 11:44:48 +0530

    layout items

--- src/components/checkout-item.jsx
+++ src/components/checkout-item.jsx
@@ -8,21 +8,24 @@ const CheckoutItem = ({ cartItem, clearItem, removeItem, addItem }) => {
   const { imageUrl, name, quantity, price } = cartItem;
 
   return (
-    <div className='checkout-item'>
-      <div className='image-container'>
-        <img src={imageUrl} alt='item' />
+    <div className="checkout-item">
+      <div className="image-container">
+        <img src={imageUrl} alt={name} />
       </div>
 
-      <span className='name'>{name}</span>
+      <span className="name">{name}</span>
 
-      <div className='right-group'>
-        <span className='quantity'>
-          <div className='arrow' onClick={() => removeItem(cartItem)}>&#10094;</div>
-          <span className='value'>{quantity}</span>
-          <div className='arrow' onClick={() => addItem(cartItem)}>&#10095;</div>
+      {/* Right section: quantity, price, and remove button always inline */}
+      <div className="right-group">
+        <span className="quantity">
+          <div className="arrow" onClick={() => removeItem(cartItem)}>&#10094;</div>
+          <span className="value">{quantity}</span>
+          <div className="arrow" onClick={() => addItem(cartItem)}>&#10095;</div>
         </span>
-        <span className='price'>{price}</span>
-        <div className='remove-button' onClick={() => clearItem(cartItem)}>
+
+        <span className="price">{price}</span>
+
+        <div className="remove-button" onClick={() => clearItem(cartItem)}>
           &#10005;
         </div>
       </div>
